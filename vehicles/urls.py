0be571876ed66458865a9034657f078ae25b4c26
Marcela <marceladeframchi@gmail.com>
2025-01-07 05:20:54 -0300

    se crean nuevas funcionalidades

--- vehicles/urls.py
+++ vehicles/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.v_index, name = 'index')
+    path('', views.v_index, name = 'index'),
+    path('vehicle/add', views.v_add, name = 'add'),
+    path('listar', views.v_listar, name = 'listar')
 ]
--- vehicles/views.py
+++ vehicles/views.py
@@ -1,5 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from vehicles.models import Vehiculo
 
 # Create your views here.
 def v_index(request):
     return render(request, 'vehicles/index.html')
+
+def v_add(request):
+    if request.method == 'POST':
+        data = request.POST.copy()
+        vehiculo = Vehiculo()
+
+        vehiculo.marca = data['marca']
+        vehiculo.modelo = data['modelo']
+        vehiculo.serial_carroceria = data['serial_carroceria']
+        vehiculo.serial_motor = data['serial_motor']
+        vehiculo.categoria = data['categoria']
+        vehiculo.precio = data['precio']
+        vehiculo.fecha_creacion = data['fecha_creacion']
+        vehiculo.fecha_modificacion = data['fecha_modificacion']
+        
+        vehiculo.save()
+        return redirect('/')
+    return render(request, 'vehicles/add.html')
+
+def v_listar(request):
+    vehiculos = Vehiculo.objects.all()
+    context = {
+        'vehiculos': vehiculos
+    }
+    return render(request, 'vehicles/listar.html', context)
